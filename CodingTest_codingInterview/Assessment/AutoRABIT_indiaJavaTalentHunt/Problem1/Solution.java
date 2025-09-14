791a01019388f5681744c79f632761682da44f59
Soyeb Ali Faruque <soyebalifaruque@gmail.com>
2025-09-15 03:38:29 +0530

    added extra folder

---
+++ CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem1/Solution.java
@@ -0,0 +1,20 @@
+package Assessment.AutoRABIT_indiaJavaTalentHunt.Problem1;
+
+public class Solution {
+    public static void main(String[] args) {
+        //test cases
+
+    }
+
+}
+class Product{
+
+}
+
+class Validator{
+
+}
+
+class InvalidCouponException extends Exception{
+
+}
---
+++ CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem2/Solution.java
@@ -0,0 +1,10 @@
+package Assessment.AutoRABIT_indiaJavaTalentHunt.Problem2;
+
+public class Solution {
+    public static void main(String[] args) {
+        //test cases
+
+    }
+}
+
+
