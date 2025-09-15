34f27693ba983c1c724b0d9f448ea9600bd1b6d6
Soyeb Ali Faruque <soyebalifaruque@gmail.com>
2025-09-16 00:27:47 +0530

    update

--- CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem1/Solution.java
+++ CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem1/Solution.java
@@ -1,5 +1,7 @@
 package Assessment.AutoRABIT_indiaJavaTalentHunt.Problem1;
 
+import java.util.StringTokenizer;
+
 public class Solution {
     public static void main(String[] args) {
         //test cases
@@ -8,13 +10,54 @@ public class Solution {
 
 }
 class Product{
+    String prodectName;
+    String coupon;
+    double price;
+    public Product(String productName,String coupon,double price){
+        this.prodectName = productName;
+        this.coupon = coupon;
+        this.price = price;
+    }
 
 }
 
 class Validator{
+    public String validateCoupon(Product p) throws InvalidCouponException{
+        if(!this.validateCouponFormat(p,p.coupon)) throw new InvalidCouponException("Invalid Coupon");
+        else return "Valid Coupon";
+
+    }
+    private boolean validateCouponFormat(Product p,String coupon){
+        if(coupon == null || !coupon.contains("-")) return false;
+        String[] parts = coupon.split("-");
+        if(!p.prodectName.equals(parts[0])) return false;
+        int discount = Integer.valueOf(parts[1]);
+        if(!(discount >= 10 && discount <= 25)) return false;
+        return true;
+
+
+    }
+
+    public double netPrice(Product p){
+        double discountedPrice = 0.0;
+        try{
+            String methodResponse = validateCoupon(p);
+            String[] parts = p.coupon.split("-");
+            double discount = Integer.valueOf(parts[1]);
+            double productPrice = p.price;
+            discountedPrice =productPrice - (discount/100*productPrice);
+
+        }catch(InvalidCouponException e){
+            System.out.println(e.getMessage());
+            return p.price;
+        }return discountedPrice;
+    }
+
 
 }
 
 class InvalidCouponException extends Exception{
-
+        public InvalidCouponException(String message){
+            super(message);
+        }
 }
--- CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem2/Solution.java
+++ CodingTest_codingInterview/Assessment/AutoRABIT_indiaJavaTalentHunt/Problem2/Solution.java
@@ -1,10 +1,61 @@
 package Assessment.AutoRABIT_indiaJavaTalentHunt.Problem2;
 
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
         //test cases
 
     }
+}
+class Employee{
+    private String name;
+    private int salary;
+    public Employee(String name,int salary){
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getSalary(){
+        return this.salary;
+    }
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+}
+
+class EmployeeInfo{
+    enum SortingMethod{
+        BYNAME,BYSALARY
+    }
+public List<Employee> sort(List<Employee> emps,final SortingMethod method){
+        if(method == SortingMethod.BYNAME) sortByName(emps);
+        else sortBySalary(emps);
+return emps;
+}
+    private void sortByName(List<Employee> emps){
+    Collections.sort(emps, Comparator.comparing(Employee::getName));
+    }
+    private void sortBySalary(List<Employee> emps){
+        Collections.sort(emps, Comparator.comparing(Employee::getSalary));
+    }
+
+    public boolean isCharacterPresentInAllNames(Collection<Employee> entities, String character){
+
+
+         for(Employee emp : entities){
+            if(!emp.getName().toLowerCase().startsWith(character.toLowerCase())) return false;
+
+         }return true;
+
+    }
+
 }
 
 
